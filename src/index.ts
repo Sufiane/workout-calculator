cc344f82eb0fb99767be160742a52f7117121206
Sufiane <su.souissi@gmail.com>
2025-05-10 22:54:37 +0200

    refactor: move business logic to service

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,4 @@
-import {
-    calculateFromMax90,
-    calculateFromMaxRm,
-    calculateFromRep85,
-    calculateFromRep90,
-    calculateFromRep95,
-} from './service';
-
-const generateBenchProgram = ({
-    maxRm,
-    max90,
-    rep85,
-    rep90,
-    rep95,
-}: {
-    maxRm?: number;
-    max90?: number;
-    rep85?: number;
-    rep90?: number;
-    rep95?: number;
-}) => {
-    if (!maxRm && !max90 && !rep85 && !rep90 && !rep95) {
-        throw new Error('min_one_ref_value_needed');
-    }
-
-    switch (true) {
-        case maxRm !== undefined:
-            return calculateFromMaxRm(maxRm);
-        case max90 !== undefined:
-            return calculateFromMax90(max90);
-        case rep95 !== undefined:
-            return calculateFromRep95(rep95);
-        case rep90 !== undefined:
-            return calculateFromRep90(rep90);
-        case rep85 !== undefined:
-            return calculateFromRep85(rep85);
-    }
-};
+import { generateBenchProgram } from './service';
 
 const handleProgram = async (query: URLSearchParams, env: Env): Promise<Response> => {
     const result = generateBenchProgram({
@@ -46,27 +9,27 @@ const handleProgram = async (query: URLSearchParams, env: Env): Promise<Response
         rep95: Number(query.get('rep95')) || undefined,
     });
 
-    await env.LATEST_VALUE.put('latest', JSON.stringify(result))
+    await env.LATEST_VALUE.put('latest', JSON.stringify(result));
 
     return Response.json(result);
-}
+};
 
 const handleFavicon = (): Response => {
     return new Response(null, { status: 204 });
-}
+};
 
 const handleLatest = async (env: Env): Promise<Response> => {
     const value = await env.LATEST_VALUE.get('latest');
 
     return Response.json(value ? JSON.parse(value) : 'no_latest_recorded');
-}
+};
 
 export default {
     async fetch(request: Request, env: Env): Promise<Response> {
         const url = new URL(request.url);
 
         if (url.pathname === '/favicon.ico') {
-            return handleFavicon()
+            return handleFavicon();
         }
 
         if (url.pathname === '/latest') {
@@ -75,6 +38,6 @@ export default {
 
         const query = url.searchParams;
 
-        return handleProgram(query, env)
+        return handleProgram(query, env);
     },
 };
--- src/service.ts
+++ src/service.ts
@@ -59,3 +59,37 @@ export function calculateFromRep85(rep85: number): Program {
         rep95: roundUp(max90 * 0.95),
     };
 }
+
+export const generateBenchProgram = ({
+    maxRm,
+    max90,
+    rep85,
+    rep90,
+    rep95,
+}: {
+    maxRm?: number;
+    max90?: number;
+    rep85?: number;
+    rep90?: number;
+    rep95?: number;
+}): Program => {
+    if (!maxRm && !max90 && !rep85 && !rep90 && !rep95) {
+        throw new Error('min_one_ref_value_needed');
+    }
+
+    switch (true) {
+        case maxRm !== undefined:
+            return calculateFromMaxRm(maxRm);
+        case max90 !== undefined:
+            return calculateFromMax90(max90);
+        case rep95 !== undefined:
+            return calculateFromRep95(rep95);
+        case rep90 !== undefined:
+            return calculateFromRep90(rep90);
+        case rep85 !== undefined:
+            return calculateFromRep85(rep85);
+        default:{
+            throw new Error('missing_params')
+        }
+    }
+};
