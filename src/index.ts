af17322a0f406f1ecdd5b7f61e8e45fd29530a77
Sufiane <su.souissi@gmail.com>
2025-05-10 21:19:58 +0200

    feat: add caching system

--- src/index.ts
+++ src/index.ts
@@ -37,25 +37,44 @@ const generateBenchProgram = ({
     }
 };
 
-export default {
-    fetch(request: Request): Response {
-        console.log('on est la on est la');
-        console.log('on est la on est la');
+const handleProgram = async (query: URLSearchParams, env: Env): Promise<Response> => {
+    const result = generateBenchProgram({
+        maxRm: Number(query.get('maxRm')) || undefined,
+        max90: Number(query.get('max90')) || undefined,
+        rep85: Number(query.get('rep85')) || undefined,
+        rep90: Number(query.get('rep90')) || undefined,
+        rep95: Number(query.get('rep95')) || undefined,
+    });
 
+    await env.LATEST_VALUE.put('latest', JSON.stringify(result))
+
+    return Response.json(result);
+}
+
+const handleFavicon = (): Response => {
+    return new Response(null, { status: 204 });
+}
+
+const handleLatest = async (env: Env): Promise<Response> => {
+    const value = await env.LATEST_VALUE.get('latest');
+
+    return Response.json(value ? JSON.parse(value) : 'no_latest_recorded');
+}
+
+export default {
+    async fetch(request: Request, env: Env): Promise<Response> {
         const url = new URL(request.url);
 
         if (url.pathname === '/favicon.ico') {
-            return new Response(null, { status: 204 });
+            return handleFavicon()
+        }
+
+        if (url.pathname === '/latest') {
+            return handleLatest(env);
         }
 
         const query = url.searchParams;
 
-        return Response.json(generateBenchProgram({
-            maxRm: Number(query.get('maxRm')) || undefined,
-            max90: Number(query.get('max90')) || undefined,
-            rep85: Number(query.get('rep85')) || undefined,
-            rep90: Number(query.get('rep90')) || undefined,
-            rep95: Number(query.get('rep95')) || undefined,
-        }));
+        return handleProgram(query, env)
     },
 };
