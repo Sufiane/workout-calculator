5284a2ad8147fdb387693d74710e8e88c1b50242
Sufiane <su.souissi@gmail.com>
2025-08-17 11:37:10 +0200

    refactor: remove console.log

--- src/utils/round.utils.ts
+++ src/utils/round.utils.ts
@@ -1,10 +1,4 @@
 export const rounding = (toRound: number): number => {
-    console.log({
-        beforeRounding: toRound,
-        afterRounding: Math.ceil(toRound / 2.5) * 2.5,
-        afterRoundingDown: Math.floor(toRound / 2.5) * 2.5,
-    });
-
     const roundingUp = Math.ceil(toRound / 2.5) * 2.5;
     const roundingDown = Math.floor(toRound / 2.5) * 2.5;
 
